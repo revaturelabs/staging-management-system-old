801a3a1384591d0552d01442ac1d29e1082f7473
chrisnellis <christopher.t.nellis@gmail.com>
2017-01-07 17:30:43 -0500

    Update StagingManagementSystemApplication.java

--- src/main/java/com/revature/sms/StagingManagementSystemApplication.java
+++ src/main/java/com/revature/sms/StagingManagementSystemApplication.java
@@ -3,6 +3,8 @@ package com.revature.sms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//This is the main executable for the Spring Boot application
+
 @SpringBootApplication
 public class StagingManagementSystemApplication {
 
