d1a3e85355dbca3959826b636e38d03abc7bbd33
Christopher Nellis <christopher.t.nellis@gmail.com>
2017-01-07 20:07:58 -0500

    added hashPassword method in test method

--- src/test/java/com/revature/sms/StagingManagementSystemApplicationTests.java
+++ src/test/java/com/revature/sms/StagingManagementSystemApplicationTests.java
@@ -1,5 +1,8 @@
 package com.revature.sms;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,16 +49,36 @@ public class StagingManagementSystemApplicationTests {
 		urr.save(new UserRole("associate"));
 		urr.save(new UserRole("admin"));
 		urr.save(new UserRole("superAdmin"));
-		ur.save(new User("admin","Admin","Admin",urr.findByName("admin"),"password"));
-		ur.save(new User("superadmin","Super","Admin",urr.findByName("superAdmin"),"password"));
-		ur.save(new User("java","Java","Johnny",urr.findByName("associate"),"password", "Java"));
-		ur.save(new User("dotnet","DotNet","Dave",urr.findByName("associate"),"password", ".NET"));
-		ur.save(new User("setDET","SDET","Sally",urr.findByName("associate"),"password", ".NET"));
-	}*/
+		ur.save(new User("admin","Admin","Admin",urr.findByName("admin"),hashPassword("password")));
+		ur.save(new User("superadmin","Super","Admin",urr.findByName("superAdmin"),hashPassword("password")));
+		ur.save(new User("java","Java","Johnny",urr.findByName("associate"),hashPassword("password"), "Java"));
+		ur.save(new User("dotnet","DotNet","Dave",urr.findByName("associate"),hashPassword("password"), ".NET"));
+		ur.save(new User("setDET","SDET","Sally",urr.findByName("associate"),hashPassword("password"), ".NET"));
+	}
 	
+	// Defunct hashPassword script. Actual hashing will take place on the client-side javascript
+	public static String hashPassword(String inputPassword) {
+		try {
+			MessageDigest md;
+			md = MessageDigest.getInstance("SHA");
+			md.update(inputPassword.getBytes());
+			byte byteData[] = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	*/
 	@Test
   	public void getUser(){
 		System.out.println(ur.findByUsername("admin"));
   	}
+	
 	
 }
