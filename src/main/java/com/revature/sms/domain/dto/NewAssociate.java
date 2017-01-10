8ddfab962454f5504ad1c708dafbe32377ece0af
Christopher Nellis <christopher.t.nellis@gmail.com>
2017-01-10 11:30:01 -0500

    added BatchType controller

---
+++ src/main/java/com/revature/sms/domain/dto/NewAssociate.java
@@ -0,0 +1,12 @@
+package com.revature.sms.domain.dto;
+
+import com.revature.sms.domain.BatchType;
+
+public class NewAssociate {
+
+	private String firstName;
+	private String lastName;
+	private BatchType batchType;
+	
+	
+}
