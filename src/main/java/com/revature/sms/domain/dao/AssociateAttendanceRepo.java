1a886c69e113fd0d0820fc3564aefa18eb2b8d96
DESKTOP-OL0UPSI\jrbas <jrbas94@gmail.com>
2017-01-10 11:39:03 -0500

    Also the attendance repo

--- src/main/java/com/revature/sms/domain/dao/AssociateAttendanceRepo.java
+++ src/main/java/com/revature/sms/domain/dao/AssociateAttendanceRepo.java
@@ -9,10 +9,25 @@ import org.springframework.stereotype.Repository;
 import com.revature.sms.domain.AssociateAttendance;
 import com.revature.sms.domain.User;
 
+/**
+ * 
+ * DAO repository for AssociateAttendance.
+ *
+ */
 @Repository
 public interface AssociateAttendanceRepo extends JpaRepository<AssociateAttendance, Integer> {
-
+	
+	/**
+	 * Returns a list of attendance records for a specific date.
+	 * @param date - Date object for the date of desired attendance records
+	 * @return List<AssociateAttendance> - List of all attendance records for associates on the given date
+	 */
 	List<AssociateAttendance> findByDate(Date date);
 	
+	/**
+	 * Returns a list of attendance records for a specific associate.
+	 * @param associate - User object for the associate of desired attendance records
+	 * @return List<AssociateAttendance> - List of all attendance records for the given associate
+	 */ 
 	List<AssociateAttendance> findByAssociate(User associate);
 }
