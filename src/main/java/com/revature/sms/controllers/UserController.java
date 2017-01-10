23435d34a7bff079635aedf1908e78b57394436a
Claudette <czar0410@gmail.com>
2017-01-10 11:25:58 -0500

    usercontroller update

--- src/main/java/com/revature/sms/controllers/UserController.java
+++ src/main/java/com/revature/sms/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.revature.sms.controllers;
 
-import java.sql.Date;
-import java.util.Calendar;
+
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -13,12 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.revature.sms.domain.AssociateAttendance;
 import com.revature.sms.domain.User;
-import com.revature.sms.domain.dao.AssociateAttendanceRepo;
 import com.revature.sms.domain.dao.UserRepo;
 import com.revature.sms.domain.dto.ResponseErrorEntity;
 import com.revature.sms.domain.dto.UserDTO;
@@ -29,26 +26,77 @@ public class UserController {
 	@Autowired
 	UserRepo userRepo;
 
-	@Autowired
-	AssociateAttendanceRepo attendanceRepo;
-
-	
 	/**
-	 * To find user by first name
+	 * To create user 
 	 * 
 	 * @param userDTO
 	 * @return
 	 */
-	@RequestMapping(value = "/findByFirstName", method = { RequestMethod.POST,
+	@RequestMapping(value = "/createUser", method = { RequestMethod.POST,
 			RequestMethod.PUT }, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Object findUserByFirstName(@RequestBody UserDTO userDTO) {
+	public @ResponseBody Object createUser(@RequestBody UserDTO userDTO) {
+
+		try {
+			// validate token and create user
+			if (isValid(userDTO.getToken())) {
+				User user=getUser(userDTO);
+				user=userRepo.save(user);
+				return new ResponseEntity<User>(user, HttpStatus.CREATED);
+			} else {
+				return new ResponseEntity<ResponseErrorEntity>(new ResponseErrorEntity("Invalid token"),
+						HttpStatus.FORBIDDEN);
+			}
+		} catch (Exception e) {
+			Logger.getRootLogger().debug("Exception while creating user", e);
+			return new ResponseEntity<ResponseErrorEntity>(
+					new ResponseErrorEntity("Problem occurred while creating user."), HttpStatus.SERVICE_UNAVAILABLE);
+		}
+
+	}
+	
+	
+	/**
+	 * To update user info
+	 * 
+	 * @param userDTO
+	 * @return
+	 */
+	@RequestMapping(value = "/updateUser", method = { RequestMethod.POST,
+			RequestMethod.PUT }, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Object updateUser(@RequestBody UserDTO userDTO) {
+
+		try {
+			// validate token and update user info
+			if (isValid(userDTO.getToken())) {
+				User user=getUser(userDTO);
+				user=userRepo.save(user);
+				return new ResponseEntity<User>(user, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<ResponseErrorEntity>(new ResponseErrorEntity("Invalid token"),
+						HttpStatus.FORBIDDEN);
+			}
+		} catch (Exception e) {
+			Logger.getRootLogger().debug("Exception while updating user info", e);
+			return new ResponseEntity<ResponseErrorEntity>(
+					new ResponseErrorEntity("Problem occurred while updating user info."), HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	/**
+	 * To retrieve user by ID
+	 * 
+	 * @param userDTO
+	 * @return
+	 */
+	@RequestMapping(value = "/retrieveUser", method = { RequestMethod.GET}, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Object retrieveUser(@RequestParam int id, @RequestBody String token) {
 
 		try {
 			// validate token and retrieve associate info
-
-			if (isValid(userDTO.getToken())) {
-				List<User> user = userRepo.findByFirstName(userDTO.getFirstName());
-				return new ResponseEntity<List<User>>(user, HttpStatus.OK);
+			if (isValid(token)) {
+				User user = userRepo.findOne(id);
+				return new ResponseEntity<User>(user, HttpStatus.OK);
 			} else {
 				return new ResponseEntity<ResponseErrorEntity>(new ResponseErrorEntity("Invalid token"),
 						HttpStatus.FORBIDDEN);
@@ -60,31 +108,29 @@ public class UserController {
 		}
 
 	}
-
+	
 	/**
-	 * To find user by batch type
+	 * To retrieve all user
 	 * 
 	 * @param userDTO
 	 * @return
 	 */
-	@RequestMapping(value = "/findByBatchType", method = { RequestMethod.POST,
-			RequestMethod.PUT }, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Object findByBatchType(@RequestBody UserDTO userDTO) {
+	@RequestMapping(value = "/retrieveAll", method = { RequestMethod.GET}, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Object retrieveAll(@RequestBody String token) {
 
 		try {
-			// validate token and retrieve associate info
-
-			if (isValid(userDTO.getToken())) {
-				List<User> user = userRepo.findByBatchType(userDTO.getBatchType());
+			// validate token and retrieve all associates info
+			if (isValid(token)) {
+				List<User> user = userRepo.findAll();
 				return new ResponseEntity<List<User>>(user, HttpStatus.OK);
 			} else {
 				return new ResponseEntity<ResponseErrorEntity>(new ResponseErrorEntity("Invalid token"),
 						HttpStatus.FORBIDDEN);
 			}
 		} catch (Exception e) {
-			Logger.getRootLogger().debug("Exception while retrieving associate info", e);
+			Logger.getRootLogger().debug("Exception while retrieving all associates info", e);
 			return new ResponseEntity<ResponseErrorEntity>(
-					new ResponseErrorEntity("Problem occurred while retrieving associate info."), HttpStatus.NOT_FOUND);
+					new ResponseErrorEntity("Problem occurred while retrieving all associates info."), HttpStatus.NOT_FOUND);
 		}
 
 	}
@@ -95,22 +141,21 @@ public class UserController {
 	 * @param userDTO
 	 * @return
 	 */
-	@RequestMapping(value = "/deleteByUsername", method = { RequestMethod.POST,
+	@RequestMapping(value = "/deleteUser", method = { RequestMethod.POST,
 			RequestMethod.PUT }, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Object deleteByUserName(@RequestBody UserDTO userDTO) {
+	public @ResponseBody Object deleteByUserName(@RequestParam int id, @RequestBody String token) {
 
 		try {
 			// validate token and delete associate
-
-			if (isValid(userDTO.getToken())) {
-				userRepo.deleteByUsername(userDTO.getUsername());
+			if (isValid(token)) {
+				userRepo.delete(id);;
 				return new ResponseEntity<User>(HttpStatus.OK);
 			} else {
 				return new ResponseEntity<ResponseErrorEntity>(new ResponseErrorEntity("Invalid token"),
 						HttpStatus.FORBIDDEN);
 			}
 		} catch (Exception e) {
-			Logger.getRootLogger().debug("Bad username", e);
+			Logger.getRootLogger().debug("User not found", e);
 			return new ResponseEntity<ResponseErrorEntity>(
 					new ResponseErrorEntity("Problem occurred while deleting user."), HttpStatus.NOT_FOUND);
 		}
@@ -130,6 +175,18 @@ public class UserController {
 		// valid = true;
 		// }
 		return valid;
+	}
+	/**
+	 * To transform UserDTO object into an User object
+	 * @param userDTO
+	 * @return
+	 */
+	
+	public User getUser(UserDTO userDTO){
+		User user = new User();
+		String[] ignoreProperties = { "token" };
+		BeanUtils.copyProperties(userDTO, user, ignoreProperties);
+		return user;
 	}
 
 }
