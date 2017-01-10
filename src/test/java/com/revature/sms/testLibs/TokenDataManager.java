1ea774ffb6bf51db93e1a048e924d861ff2b2186
Alexander Sage <alexander.sage@jacks.sdstate.edu>
2017-01-10 15:12:38 -0500

    added UserDataManager

---
+++ src/test/java/com/revature/sms/testLibs/TokenDataManager.java
@@ -0,0 +1,17 @@
+package com.revature.sms.testLibs;
+
+
+/**
+ * 
+ * @author Sage
+ *
+ * 
+ * class TokenDataManager is used to create new authorization tokens and delete them after
+ * the tests are executed. It's essential that all setup methods record any changes made to
+ * the database so that cleanup methods can be called to undo those changes.  
+ * 
+ */
+
+public class TokenDataManager {
+
+}
