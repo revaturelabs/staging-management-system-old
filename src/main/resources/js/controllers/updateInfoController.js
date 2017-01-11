d811248ab58f8be2dc50c39172a52b1bf1df29d8
Juan Carmona <juancarmona93@yahoo.com>
2017-01-11 16:57:22 -0500

    almost there

--- src/main/resources/js/controllers/updateInfoController.js
+++ src/main/resources/js/controllers/updateInfoController.js
@@ -1,5 +1,5 @@
  var sms = angular.module( "sms" );
-sms.controller( "updateInfoCrtl", function( $scope, $state, $mdSidenav, loginService){
+sms.controller( "updateInfoCrtl", function( $scope, $state, $mdSidenav, loginService, $http){
 	var uic = this;
     	uic.oldPass = "";
     	uic.newPass = "";
@@ -13,25 +13,55 @@ sms.controller( "updateInfoCrtl", function( $scope, $state, $mdSidenav, loginSer
     	
  
     	
-    	console.log(newPassH + " : " +  confirmPassH);
+    	console.log(oldPassH + " : " +newPassH + " : " +  confirmPassH);
     	if(newPassH == confirmPassH){
-    		//new passwords match, make call to server
-    		console.log("Passwords match");
-    		
-    		// data
-            uic.user = loginService.getUser();
-            uic.token = loginService.getToken();
-            
-            // URL: /api/v1/login
-            //use: PUT
-            uic.UserDTO = {"username": username, "hashedPassword": oldPassH};
-            
-            
-    		
+    		if(oldPassH != newPassH){
+	    		//new passwords match, make call to server
+	    		console.log("Passwords match");
+	    		
+	    		// data
+	            uic.user = loginService.getUser();
+	            //uic.token = loginService.getToken();
+	            uic.token = "7fp;^0Ssm2g[_RM8PI<rkBhPm6<5:1b9DA9A2N:<";
+	            
+	            //old and new passwords not be the same
+	            
+	            // URL: /api/v1/login
+	            //use: PUT
+	            //Header: Authorization: token
+	            
+	            //uic.userDTO = {"username": uic.user.username, "oldPassword": oldPassH, "newPassword":newPassH};
+	            uic.userDTO = {"username": uic.user.username, "oldPassword": oldPassH, "newPassword":newPassH};
+	           
+	            console.log("token: " + uic.token);
+	            console.log("username: " + uic.user.username);
+	            
+	            console.log("UserDto" + JSON.stringify(uic.userDTO));
+	            
+	            $http({
+	            	  method: 'PUT',
+	            	  url: '/api/v1/login',
+	            	  headers:{'Authorization' : uic.token,
+	            		  "Content-Type":"application/json"
+	            	  },
+	      
+	            	  data:{
+	            		  "userDTO" : uic.userDTO
+	            	  }
+	            	}).then(function successCallback(response) {
+
+	            		console.log("password changed successfully");
+	            	  }, function errorCallback(response) {
+	            		  console.log("password change went wrong. " + response.status + " : "+ response.statusText);
+	            	  });
+    		}else{
+    			//old password and new password are the same
+    			console.log("Old password and new pasword are the same.");
+    		}
     	}
     	else{
     		// passwords don't match
-    		console.log("Passwords Don't Match");
+    		console.log("password confirmation does not match.");
     	}
     	
     	oldPassH = "";
