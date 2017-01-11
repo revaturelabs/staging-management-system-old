df4b0a2965b7e3b1b6b091188fc5967cd5978e7e
Juan Carmona <juancarmona93@yahoo.com>
2017-01-11 15:13:36 -0500

    making more progress

--- src/main/resources/js/controllers/updateInfoController.js
+++ src/main/resources/js/controllers/updateInfoController.js
@@ -7,20 +7,35 @@ sms.controller( "updateInfoCrtl", function( $scope, $state, $mdSidenav, loginSer
     uic.submit = function(){
     	console.log("Data submitted");
     	console.log("oldPass: " + oldPass.value);
-    	/*uic.oldPassH = CryptoJS.SHA1(oldPass.value).toString();
-    	uic.newPassH = CryptoJS.SHA1(newPass.value).toString();
-    	uic.confirmPassH = CryptoJS.SHA1(confirmPass.value).toString();*/
+    	var oldPassH = CryptoJS.SHA1(oldPass.value).toString();
+    	var newPassH = CryptoJS.SHA1(newPass.value).toString();
+    	var confirmPassH = CryptoJS.SHA1(confirmPass.value).toString();
     	
  
     	
-    	
-    	if(uic.newPass == uic.confirmPass){
+    	console.log(newPassH + " : " +  confirmPassH);
+    	if(newPassH == confirmPassH){
     		//new passwords match, make call to server
     		console.log("Passwords match");
+    		
+    		// data
+            uic.user = loginService.getUser();
+            uic.token = loginService.getToken();
+            
+            // URL: /api/v1/login
+            //use: PUT
+            uic.UserDTO = {"username": username, "hashedPassword": oldPassH};
+            
+            
+    		
     	}
     	else{
     		// passwords don't match
     		console.log("Passwords Don't Match");
     	}
+    	
+    	oldPassH = "";
+    	newPassH = "";
+    	confirmPassH = "";
     }
 });
