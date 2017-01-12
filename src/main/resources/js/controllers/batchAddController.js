5bfc2d0bc3e1afc13f48e124186c1293d9dbf387
Joseph Tamberino <jtamber2@gmail.com>
2017-01-12 15:34:11 -0500

    associates can be added with batch

--- src/main/resources/js/controllers/batchAddController.js
+++ src/main/resources/js/controllers/batchAddController.js
@@ -1,57 +1,62 @@
+var sms = angular.module("sms");
 
-    var sms = angular.module( "sms" );
+sms.controller("batchAddCtrl", function($scope, $mdDialog, userService,
+		batchTypeService) {
+	var bac = this;
 
-    sms.controller( "batchAddCtrl", function( $scope, $mdDialog, userService, batchTypeService ) {
-        var bac = this;
+	// functions
+	// adds new associate to list
 
-          // functions
-            // adds new associate to list
+	bac.addNew = function(isValid) {
+		if (isValid) {
+			bac.associates.push({
+				firstName : bac.firstName,
+				lastName : bac.lastName
+			});
+			bac.firstName = "";
+			bac.lastName = "";
+			$scope.newAssociate.$setUntouched();
+			$scope.newAssociate.$setPristine();
+			angular.element("#firstName").focus();
+		}
+	};
 
-        bac.addNew = function(isValid) {
-            if (isValid) {
-                bac.associates.push( { firstName: bac.firstName, lastName: bac.lastName, batchType: bac.selectedBatchType } );
-                bac.firstName = "";
-                bac.lastName = "";
-                $scope.newAssociate.$setUntouched();
-                $scope.newAssociate.$setPristine();
-                angular.element("#firstName").focus();
-            }
-        };
+	// data
+	bac.associates = [];
+	bac.batchTypes = batchTypeService.getAll(function(response) {
+		bac.batchTypes = response;
+	}, function(error) {
+		$mdDialog.cancel();
+	});
+	// Hard coded value for userRole object of associate
+	var userRole = {};
+	userRole.name = "associate";
+	userRole.id = 1;
 
-          // data
-        bac.associates = [];
-        bac.batchTypes = batchTypeService.getAll( function(response){
-            bac.batchTypes = response;
-        }, function(error){
-            $mdDialog.cancel();
-        });
-        //Hard coded value for userRole object of associate
-        var userRole = {};
-		userRole.name = "associate";
-		userRole.id = 1;
-		
+	bac.save = function(isValid, list) {
+		if (isValid) {
+			if (list.length != 0) {
+				
+				var addUser = list.shift();
+				addUser.batchType = bac.selectedBatchType;
+				addUser.username = addUser.firstName.substring(0, 1)
+						.toLowerCase()
+						+ addUser.lastName.toLowerCase();
+				addUser.hashedPassword = CryptoJS.SHA1(addUser.username)
+						.toString();
+				addUser.userRole = userRole;
+				// call rest controller to save user via userService
+				userService.create(addUser, function(response) {
+				}, function(error) {
+				});
+				this.save(isValid,list);
+			} else {
+				$mdDialog.hide();
+			}
+		}
+	}
 
-		
-        bac.save = function(list){
-        	if (list.length != 0){
-        		var addUser = list.shift();
-        		addUser.username = addUser.firstName.substring(0,1).toLowerCase()
-        		+ addUser.lastName.toLowerCase();
-        		addUser.hashedPassword = CryptoJS.SHA1(addUser.username).toString();
-        		addUser.userRole = userRole;
-        		//call rest controller to save user via userService
-        		userService.create(addUser, function(response){        			
-        		}, function(error){        			
-        		});
-        		this.save(list);
-        	}
-        	else {
-        		$mdDialog.hide();
-        	}
-        	
-        }
-        
-        bac.cancel = function(){
-        	$mdDialog.cancel();
-        }
-    });
+	bac.cancel = function() {
+		$mdDialog.cancel();
+	}
+});
