c4f13cfb2fb3640e3471203b576e4436197c5e8b
Christopher Nellis <christopher.t.nellis@gmail.com>
2017-01-13 10:25:55 -0500

    Fixing poor Charles's merge conflict

--- src/main/resources/js/controllers/loginController.js
+++ src/main/resources/js/controllers/loginController.js
@@ -23,7 +23,7 @@ sms.controller("loginCtrl", function($scope, $state, loginService) {
 					lc.toast("Logged in.");
 					break;
 				case "admin":
-					$state.go("admin");
+					$state.go("adminAttendance");
 					lc.toast("Logged in.");
 					break;
 				case "associate":
