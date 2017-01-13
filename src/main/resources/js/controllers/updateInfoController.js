9e339980f6be11b07e4fb4886cc6bc100c9d9cf8
Juan Carmona <juancarmona93@yahoo.com>
2017-01-13 10:25:19 -0500

    edited change password routing, upon canceling or successfully updating password

--- src/main/resources/js/controllers/updateInfoController.js
+++ src/main/resources/js/controllers/updateInfoController.js
@@ -11,9 +11,9 @@ sms.controller( "updateInfoCrtl", function( $scope, $state, $mdSidenav, loginSer
     uic.cancel = function(){
     	//route to the appropriate homepage
 		switch(loginService.getUser().userRole.name){
-		case "associate":$state.go("assoc"); break;
+		case "associate":$state.go("assocAttendance"); break;
 		case "admin" : $state.go("admin"); break;
-		case "superAdmin" : $state.go("super"); break;
+		case "superAdmin" : $state.go("superAttendance"); break;
 		}
     }
     
@@ -60,9 +60,9 @@ sms.controller( "updateInfoCrtl", function( $scope, $state, $mdSidenav, loginSer
 	            		
 	            		//route to the appropriate homepage
 	            		switch(response.data.userRole.name){
-	            		case "associate":$state.go("assoc"); break;
+	            		case "associate":$state.go("assocAttendance"); break;
 	            		case "admin" : $state.go("admin"); break;
-	            		case "superAdmin" : $state.go("super"); break;
+	            		case "superAdmin" : $state.go("superAttendance"); break;
 	            		}
 	            		
 	            	}, function errorCallback(response) {
