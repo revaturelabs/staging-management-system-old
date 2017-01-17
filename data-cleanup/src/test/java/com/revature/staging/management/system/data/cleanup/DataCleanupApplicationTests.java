e91ee0cd299c225ac53850cdc236c0e263569fb3
Christopher Nellis <christopher.t.nellis@gmail.com>
2017-01-17 13:05:55 -0500

    beginning work

---
+++ data-cleanup/src/test/java/com/revature/staging/management/system/data/cleanup/DataCleanupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revature.staging.management.system.data.cleanup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataCleanupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
