66f82134247597de9de3cdbcd08ca6f4815ccf34
Ben <benorosz@gmail.com>
2017-01-19 10:53:26 -0500

    Modified state structure. Have yet to find out what havok that wrought.

--- src/main/resources/js/sms.js
+++ src/main/resources/js/sms.js
@@ -8,95 +8,161 @@
 		$urlRouterProvider.otherwise("/login");
 
 		$stateProvider
-              // login
-			.state( "login", {
-				url: "/login",
-                views: {
-                    "mainView": {
-    				    templateUrl: "html/views/login.html",
-	    			    controller: "loginCtrl as logCtrl"
-                    }
-                }
-			})
-               
-              // superadmin page
-			.state( "super", {
-				url: "/super",
+              
+              // root
+            .state( "root", {
+                url: "",
+                abstract: true,
+                template: "<ui-view/>",
                 views: {
                     "topBarView": {
                         templateUrl: "html/views/topBar.html"
-                    },
-                    "mainView": {
-    				    templateUrl: "html/views/superadmin/super.html",
-	    			    controller: "superCtrl as suCtrl"
                     }
                 }
-			})
-                  // superadmin view all attendance
-                .state( "superAttendance", {
-                    url: "^/attendance",
-                    parent: "super",
+            })
+
+                  // login
+                .state( "root.login", {
+                    url: "/login",
                     views: {
-                        "mainSuperView": {
-                            templateUrl: "html/views/superadmin/superAttendance.html",
-                            controller: "superAttendanceCtrl as supAttCtrl"
+                        "mainView": {
+                            templateUrl: "html/views/login.html",
+                            controller: "loginCtrl as logCtrl"
+                        },
+                        "topBarView": {
+                            template: ""
                         }
-                        
                     }
                 })
 
-              // admin page
-			.state( "admin", {
-				url: "/admin",
-                views: {
-                    "topBarView": {
-                        templateUrl: "html/views/topBar.html"
-                    },
-                    "mainView": {
-    				    templateUrl: "html/views/admin/admin.html",
-	    			    controller: "adminCtrl as adCtrl"
-                    }
-                }
-			})
-				 // admin view all attendance
-	                .state( "adminAttendance", {
-	                    url: "/attendance",
-	                    parent: "admin",
-	                    views: {
-	                        "mainAdminView": {
-	                            templateUrl: "html/views/admin/associate-attendance-table.html",
-	                            controller: "adminAttendanceCtrl as adAttCtrl"
-	                        }
-	                    }
-	                })
+                  // attendance
+                .state( "root.attendance", {
+                    url: "/attendance",
+                    abstract: true,
+                    template: "<ui-view/>"
+                })
+
+                      // manager attendance
+                    .state( "root.attendance.managerAttendance", {
+                        url: "",
+                        abstract: true,
+                        template: "<ui-view/>"
+                    })
+
+                          // superadmin attendance
+                        .state( "root.attendance.managerAttendance.superadminAttendance", {
+                            url: "",
+                            views: {
+                                "mainView": {
+                                    templateUrl: "html/views/superadmin/superAttendance.html",
+                                    controller: "superAttendanceCtrl",
+                                    controllerAs: "supAttCtrl"
+                                }
+                            }
+                        })
+
+                          // admin attendance
+                        .state( "root.attendance.managerAttendance.adminAttendance", {
+                            url: "",
+                            views: {
+                                "mainView": { 
+                                    templateUrl: "html/views/admin/adminAttendance.html",
+                                    controller: "adminAttendanceCtrl",
+                                    controllerAs: "adAttCtrl"
+                                }
+                            }
+                        })
+                    
+                      // associate attendance
+                    .state( "root.attendance.associateAttendance", {
+                        url: "",
+                        views: {
+                            "mainView": {
+                                templateUrl: "html/views/associate/associateAttendance.html",
+                                controller: "associateWeeklyAttendenceCtrl",
+                                controllerAs: "assWeekAttCtrl"
+                            } 
+                        }
+                    })
+
+            //   // superadmin page
+			// .state( "super", {
+			// 	url: "/super",
+            //     views: {
+            //         "topBarView": {
+            //             templateUrl: "html/views/topBar.html"
+            //         },
+            //         "mainView": {
+    		// 		    templateUrl: "html/views/superadmin/super.html",
+	    	// 		    controller: "superCtrl as suCtrl"
+            //         }
+            //     }
+			// })
+            //       // superadmin view all attendance
+            //     .state( "superAttendance", {
+            //         url: "^/attendance",
+            //         parent: "super",
+            //         views: {
+            //             "mainSuperView": {
+            //                 templateUrl: "html/views/superadmin/superAttendance.html",
+            //                 controller: "superAttendanceCtrl as supAttCtrl"
+            //             }
+                        
+            //         }
+            //     })
+
+            //   // admin page
+			// .state( "admin", {
+			// 	url: "/admin",
+            //     views: {
+            //         "topBarView": {
+            //             templateUrl: "html/views/topBar.html"
+            //         },
+            //         "mainView": {
+    		// 		    templateUrl: "html/views/admin/admin.html",
+	    	// 		    controller: "adminCtrl as adCtrl"
+            //         }
+            //     }
+			// })
+			// 	 // admin view all attendance
+	        //         .state( "adminAttendance", {
+	        //             url: "/attendance",
+	        //             parent: "admin",
+	        //             views: {
+	        //                 "mainAdminView": {
+	        //                     templateUrl: "html/views/admin/associate-attendance-table.html",
+	        //                     controller: "adminAttendanceCtrl as adAttCtrl"
+	        //                 }
+	        //             }
+	        //         })
 			
-              // associate page
-			.state( "assoc", {
-				url: "/assoc",
-                views: {
-                    "topBarView": {
-                        templateUrl: "html/views/topBar.html"
-                    },
-                    "mainView": {
-    				    templateUrl: "html/views/associate/associate.html",
-	    			    controller: "associateCtrl as assoCtrl"
-                    }
-                }
-			})
+            //   // associate page
+			// .state( "assoc", {
+			// 	url: "/assoc",
+            //     views: {
+            //         "topBarView": {
+            //             templateUrl: "html/views/topBar.html"
+            //         },
+            //         "mainView": {
+    		// 		    templateUrl: "html/views/associate/associate.html",
+	    	// 		    controller: "associateCtrl as assoCtrl"
+            //         }
+            //     }
+			// })
 
-			//.....................................................................
-			// superadmin view all attendance
-                .state( "assocAttendance", {
-                    url: "^/weeklyattendence",
-                    parent: "assoc",
-                    views: {
-                        "mainAssociateView": {
-                            templateUrl: "html/views/associate/associateWeeklyAttendence.html",
-                            controller: "associateWeeklyAttendenceCtrl as assWeekAttCtrl"
-                        }
+			// //.....................................................................
+			// // superadmin view all attendance
+            //     .state( "assocAttendance", {
+            //         url: "^/weeklyattendence",
+            //         parent: "assoc",
+            //         views: {
+            //             "mainAssociateView": {
+            //                 templateUrl: "html/views/associate/associateWeeklyAttendence.html",
+            //                 controller: "associateWeeklyAttendenceCtrl as assWeekAttCtrl"
+            //             }
                         
-                    }
-                })
+            //         }
+            //     })
 			//........................................................................
 
 			
@@ -104,38 +170,38 @@
              * User can change their password
              */
             // superAdmin update information
-                .state( "SUupdateInfo", {
-                    url: "^/updateInformation-sa",
-                    parent: "super",
-                    views: {
-                        "mainSuperView": {
-                            templateUrl: "html/views/updateInformation.html",
-                            controller: "updateInfoCrtl as uInfoctrl"
-                        }
-                    }
-                })
-            // Admin update information
-                .state( "ADupdateInfo", {
-                    url: "^/updateInformation-a",
-                    parent: "admin",
-                    views: {
-                        "mainAdminView": {
-                            templateUrl: "html/views/updateInformation.html",
-                            controller: "updateInfoCrtl as uInfoctrl"
-                        }
-                    }
-                })
-			// Associate update information
-                .state( "ASupdateInfo", {
-                    url: "^/updateInformation-as",
-                    parent: "assoc",
-                    views: {
-                        "mainAssociateView": {
-                            templateUrl: "html/views/updateInformation.html",
-                            controller: "updateInfoCrtl as uInfoctrl"
-                        }
-                    }
-                })
+            //     .state( "SUupdateInfo", {
+            //         url: "^/updateInformation-sa",
+            //         parent: "super",
+            //         views: {
+            //             "mainSuperView": {
+            //                 templateUrl: "html/views/updateInformation.html",
+            //                 controller: "updateInfoCrtl as uInfoctrl"
+            //             }
+            //         }
+            //     })
+            // // Admin update information
+            //     .state( "ADupdateInfo", {
+            //         url: "^/updateInformation-a",
+            //         parent: "admin",
+            //         views: {
+            //             "mainAdminView": {
+            //                 templateUrl: "html/views/updateInformation.html",
+            //                 controller: "updateInfoCrtl as uInfoctrl"
+            //             } 
+            //         }
+            //     })
+			// // Associate update information
+            //     .state( "ASupdateInfo", {
+            //         url: "^/updateInformation-as",
+            //         parent: "assoc",
+            //         views: {
+            //             "mainAssociateView": {
+            //                 templateUrl: "html/views/updateInformation.html",
+            //                 controller: "updateInfoCrtl as uInfoctrl"
+            //             }
+            //         }
+            //     })
 
 	});
 
