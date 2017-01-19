6dbe49497af41dd278c90be7eecd641ba075d3e7
Corey <clshaps93@gmail.com>
2017-01-19 13:16:13 -0500

    added wait after driver.get()s.

--- src/test/java/com/revature/sms/util/EventListener.java
+++ src/test/java/com/revature/sms/util/EventListener.java
@@ -7,6 +7,15 @@ import org.openqa.selenium.support.events.*;
 //Activates thread sleeping after certain WebDriver methods so that tests are easier to follow by humans
 public class EventListener extends AbstractWebDriverEventListener {
 
+	@Override
+	public void afterNavigateTo(String url, WebDriver driver) {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	@Override
 	public void afterNavigateBack(WebDriver driver) {
