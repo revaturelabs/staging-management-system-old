21ac7be11361564559b2593e153ad2d83cb9efbb
NaMonta Gaines <Gaines.NaMonta@gmail.com>
2017-01-23 11:22:23 -0500

    added Status table and POJO

---
+++ src/main/java/com/revature/sms/domain/Status.java
@@ -0,0 +1,56 @@
+package com.revature.sms.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="STATUS")
+public class Status {
+
+	@Id
+	@Column(name = "ID")
+	@SequenceGenerator(allocationSize = 1, name = "statusSeq", sequenceName = "STATUS_SEQ")
+	@GeneratedValue(generator = "statusSeq", strategy = GenerationType.SEQUENCE)
+	private int ID;
+	
+	@Column(name="STATUS_NAME", nullable=false)
+	private String name;
+
+	public Status() {
+		super();
+	}
+
+	public Status(int iD, String name) {
+		super();
+		ID = iD;
+		this.name = name;
+	}
+
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Status [ID=" + ID + ", name=" + name + "]";
+	}
+
+}
