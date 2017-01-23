9bc3715b034835805714e90ecc16ff766cfe6c71
Ben <benorosz@gmail.com>
2017-01-23 14:19:27 -0500

    Small things.

--- src/main/resources/js/controllers/global/rootController.js
+++ src/main/resources/js/controllers/global/rootController.js
@@ -6,13 +6,13 @@
     function rootCtrl( $scope, $state, $location, $mdToast ) {
         var rc = this;
         
-            // bindables
+          // bindables
             // data
             // functions
 
-            // initializations
-            // functions
-            // listeners
+          // initializations
+          // functions
+          // listeners
             // calls root-level toast function
         $scope.$on( "toastMessage", function( events, message ) {
             $mdToast.show( $mdToast.simple().textContent( message ).action("OKAY").position("top right").highlightAction(true) );
