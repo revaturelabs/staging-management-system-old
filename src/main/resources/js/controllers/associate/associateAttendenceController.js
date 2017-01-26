c398a7ca454a694ede87cfb229984495a5950717
Joseph Tamberino <jtamber2@gmail.com>
2017-01-26 16:46:59 -0500

    more associateAttendanceController updates

--- src/main/resources/js/controllers/associate/associateAttendenceController.js
+++ src/main/resources/js/controllers/associate/associateAttendenceController.js
@@ -6,6 +6,8 @@
  * @description AngularJs controller for associate attendance functionality.
  */
     function associateAttendanceCtrl( $mdDialog, $scope, $state, $filter, loginService, userService, weekdays ) {
+       
+       
         var aac = this;
 
           // bindables
@@ -23,16 +25,27 @@
         aac.maxWeek = new Date( aac.curr.getFullYear(), aac.curr.getMonth(), aac.curr.getDate() + 7 );
 
             // functions
+        /**@var {function} calcWeek function reference variable. */
         aac.calcWeek = calcWeek;
+         /**@var {function} setToolbar function reference variable. */
         aac.setToolbar = setToolbar;
+         /**@var {function} openEvents function reference variable. */
         aac.openEvents = openEvents;
+         /**@var {function} assocCertifications function reference variable. */
         aac.assocCertifications = assocCertifications;
+         /**@var {function} getScheduledCert function reference variable. */
         aac.getScheduledCert = getScheduledCert;
+        /**@var {function} getScheduledCert function reference variable. */
         aac.days_between = days_between;
+        /**@var {function} getScheduledCert function reference variable. */
         aac.isSameDate = isSameDate;
+        /**@var {function} getScheduledCert function reference variable. */
         aac.prevWeek = prevWeek;
+        /**@var {function} getScheduledCert function reference variable. */
         aac.nextWeek = nextWeek;
+        /**@var {function} getScheduledCert function reference variable. */
         aac.toast = toast;
+        /**@var {function} getScheduledCert function reference variable. */
         aac.checkIn = checkIn;
 
           // initialization
@@ -44,6 +57,10 @@
         
           // functions
             // returns list of date objects representing the week
+
+            /**
+             * @description Creates a list of date objects to represent the week.
+             */
         function calcWeek( date ) {
             
             var monday = new Date( date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 1 );
@@ -68,6 +85,10 @@
         }
 
           // SonarQube appeasement
+
+          /**
+           * @description Changes the users status from CheckedIn to Absent. Not entirely sure why this is an option but it's here.
+           */
         function dialogYes() {
 	    	//selected yes
 	    	//checkout
@@ -78,14 +99,20 @@
 	    		aac.setToolbar();
 	    	});
 		 }
-		 
+		 /**
+          * @description Method that runs when update was successful.
+          */
         function updateSuccess(){ 
 			aac.toast("Successfully checked in.")
     		aac.calcWeek( aac.curr );
     		aac.setToolbar();
     	}
           //end sonarQube appeasement
-        
+        /**
+         * @description Determines if the logged in user has checked in today or not,
+         * and adjusts an options to be added to the actiont toolbar accordingly.
+         * @return {object} Option for the toolbar.
+         */
         function todayCheckedIn(){
         	var d = new Date();
         	for(var i=0; i< aac.user.attendance.length; i++){
@@ -119,7 +146,10 @@
         	return {"function": aac.checkIn, "icon": "check", "tooltip": "Check in"};
         }
 
-            // sets toobar icons and functions
+            
+            /**
+             * @description Sets the tooblar functions for an associate.
+             */
         function setToolbar() {
         	var actions=[];
         	
@@ -147,6 +177,9 @@
         }
 
             // opens dialog to display user events
+            /**
+             * @description Opens the dialog to display events for the logged in associate.
+             */
         function openEvents() {
             $mdDialog.show({
                 templateUrl: "html/templates/viewEvents.html",
@@ -157,7 +190,9 @@
                 escapeToClose: true
             });
         }
-
+        /**
+         * @description Displays the dialog window for scheduling a certification.
+         */
         function assocCertifications() {
             if (getScheduledCert() == null) {
                 $mdDialog.show({
@@ -173,7 +208,12 @@
                 aac.toast("You can only schedule one certification at a time.");
             }
         }
-        
+        /**
+         * @description Determines the difference betwen the two supplied dates.
+         * @param {date} date1 First supplied date.
+         * @param {date} date2 Second supplied date.
+         * @returns {number} Number of days between the two dates
+         */
         function days_between(date1, date2) {
 
             // The number of milliseconds in one day
@@ -191,6 +231,10 @@
 
         }
         
+        /**
+         * @description Checks to see if the date supplied is the same date is the same as today.
+         * @returns {boolean} Result
+         */
         function isSameDate(date) {
         	return (
         		aac.today.getFullYear() == date.getFullYear() &&
@@ -200,7 +244,10 @@
         }
         
 
-          // if the user has a scheduled cert, return the formatted date of that cert, otherwise return null
+          /**
+           * @description Gets the date of the schedule certification if it exists.
+           * @returns {string|null} Formatted date of the certification if it exists, null if one doesn't.
+           */
         function getScheduledCert() {
         	for(var i = 0; i < aac.user.tasks.length; i++) {
         		var certDate = new Date(aac.user.tasks[i].date);
@@ -224,7 +271,10 @@
         	return null;
         }
 
-         // checks if previous week is before minimum date and resets week dates if not
+         /**
+          * @description Goes to the previous week in the attendance display, unless the display is on the minimum
+          * week already.
+          */
         function prevWeek() {
             var newDate = new Date( aac.curr.getFullYear(), aac.curr.getMonth(), aac.curr.getDate() - 7 );
             if ( newDate.getTime() < aac.minWeek.getTime() ) {
@@ -235,7 +285,11 @@
             }
         }
 
-            // checks if next week is after maximum date and resets week dates if not
+            
+        /**
+          * @description Goes to the next week in the attendance display, unless the display is on the maximum
+          * week already.
+          */
         function nextWeek() {
             var newDate = new Date( aac.curr.getFullYear(), aac.curr.getMonth(), aac.curr.getDate() + 7 );
             if ( newDate.getTime() > aac.maxWeek.getTime() ) {
@@ -246,7 +300,10 @@
             }
         }
         
-        // marks an associate as checked in.
+        
+        /**
+         * @description Marks the associate as checked in, or shows a dialog asking if the associate wants to be marked as absent.
+         */
         function checkIn(){
         	var d = new Date();
         	//find current attendance object
@@ -280,7 +337,10 @@
         	}
         }
 
-            // calls root-level toast function
+         /**
+           * @description Displays a toast notification.
+           * @param {string} message The value of the message to be shown.
+           */
         function toast( message ) {
             $scope.$emit( "toastMessage", message );
         }
