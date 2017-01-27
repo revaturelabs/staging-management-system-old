4c356665e931374eb3564a186dfaf42f3daaecb5
Joseph Tamberino <jtamber2@gmail.com>
2017-01-27 15:26:55 -0500

    services documented

--- src/main/resources/js/services/skillService.js
+++ src/main/resources/js/services/skillService.js
@@ -1,8 +1,11 @@
  angular
         .module( "sms" )
         .service( "skillService", skillService );
-        
+    /**
+     * @description AngularJs service for skills
+     */    
     function skillService( $resource,loginService ) {
+        /**@var {object} Resoruce object for connecting to the REST service */
         var skillResource = $resource("/api/v1/TechSkills");
         var ssr = this;
 
@@ -12,7 +15,13 @@
 //                    get   : { headers: { "Content-Type": "application/json"} }
 //                } 
 //            )
-//            
+//         
+
+        /**
+         * @description Gets all skills from the database.
+         * @param {function} success Function that runs on successful retrieval.
+         * @param {function} error Function that runs on failed retrieval.
+         */   
         ssr.getAll = function(success,error){
         	skillResource.query(success,error);
         }
--- src/main/resources/js/services/userService.js
+++ src/main/resources/js/services/userService.js
@@ -2,9 +2,12 @@
     angular
         .module( "sms" )
         .service( "userService", userService );
-        
+        /**
+         * @description AngularJs service for users.
+         */
     function userService( $resource, loginService ) {
         var us = this;
+        /**@var {object} Resoruce object for connecting to the REST service */
         us.userResource = $resource("api/v1/user/:username", 
             { id: "@username" }, 
             { 
@@ -15,23 +18,47 @@
                 remove: { headers: { "Content-Type": "application/json", "Authorization": loginService.getToken() } } 
             } 
         )
-
+       /**
+         * @description Creates user in the database.
+         * @param {object} user The user to create.
+         * @param {function} success Function that runs on successful retrieval.
+         * @param {function} error Function that runs on failed retrieval.
+         */
         us.create = function(user, success, error) {
         	us.userResource.save(user, success, error);
         }
-
+        /**
+         * @description Gets all users from the database.
+         * @param {function} success Function that runs on successful retrieval.
+         * @param {function} error Function that runs on failed retrieval.
+         */
         us.getAll = function(success, error) {
             us.userResource.query(success, error);
         }
-
+        /**
+         * @description Gets a single user from the database.
+         * @param {string} username The username of the user to get the information of
+         * @param {function} success Function that runs on successful retrieval.
+         * @param {function} error Function that runs on failed retrieval.
+         */
         us.retrieve = function(username, success, error) {
             userResource.get({username: username}, success, error);
         }
-
+        /**
+         * @description Updates user in the database.
+         * @param {object} user The user to update.
+         * @param {function} success Function that runs on successful retrieval.
+         * @param {function} error Function that runs on failed retrieval.
+         */
         us.update = function(user, success, error) {
             us.userResource.update(user, success, error);
         }
-
+        /**
+         * @description Removes user from the database.
+         * @param {object} user The user to delete.
+         * @param {function} success Function that runs on successful retrieval.
+         * @param {function} error Function that runs on failed retrieval.
+         */
         us.remove = function(user, success, error) {
             user.$remove(success, error);
         }
