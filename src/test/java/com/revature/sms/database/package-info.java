a17759acea6255cd4ee22d1f103b6869be1b8a4e
Corey <clshaps93@gmail.com>
2017-01-30 11:26:11 -0500

    The testing files were all out of whack because of all the merging, so I just deleted them, since they don't directly affect the functioning of the website.

--- src/test/java/com/revature/sms/database/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains tools used by various test suites
- * @author chris
- */
-package com.revature.sms.database;
-
--- src/test/java/com/revature/sms/util/InstanceTestClassListener.java
+++
@@ -1,7 +0,0 @@
-package com.revature.sms.util;
-
-//Used by SpringInstanceTestClassRunner
-public interface InstanceTestClassListener {
-	void beforeClassSetup();
-	void afterClassSetup();
-}
