2d4f47f0461799741862811e2afbbb4326bdf8f5
Yureikage <Randall.charles.c@gmail.com>
2017-02-03 14:01:16 -0500

    SMS-92 (#104)
    * created dummy.txt
    
    * deleted dummy.txt
    
    * slack notification sys
    
    * Check it out! Working slacksgit status

---
+++ Slack-Notification-App-System/src/main/java/com/revature/sms/snas/SlackBot.java
@@ -0,0 +1,46 @@
+package com.revature.sms.snas;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class SlackBot {
+
+	private final static String WEBHOOK_URL = System.getenv("SLACK_WEBHOOK_URL");
+	private final static String USER_AGENT = "Mozilla/5.0";
+
+	public void sendMessage(String message) {
+		try {
+			URL obj = new URL(WEBHOOK_URL);
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", USER_AGENT);
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(message);
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			System.out.println(response);
+			System.out.println(responseCode);
+			in.close();
+		} catch (Exception ex) {
+
+		}
+	}
+}
---
+++ Slack-Notification-App-System/src/main/java/com/revature/sms/snas/domain/dao/JobEventRepo.java
@@ -0,0 +1,32 @@
+package com.revature.sms.snas.domain.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.sms.snas.domain.JobEvent;
+import com.revature.sms.snas.domain.JobEventType;
+import com.revature.sms.snas.domain.User;
+/**
+ * 
+ * DAO repo for JobEvent.
+ *
+ */
+@Repository
+public interface JobEventRepo extends JpaRepository<JobEvent, Integer>{
+/**
+ * Method to retrieve JobEvent list by specific associate.
+ * @param associate User object that matches to the list of job events to retrieve.
+ * @return List of JobEvents associated with the supplied user.
+ */
+	List<JobEvent> findByAssociate(User associate);
+
+	/**
+	 * Method to retrieve JobEvent list by JobEventType.
+	 * @param type JobEventType object that matches to the list of job events to retrieve.
+	 * @return List of JobEvents of the specific JobEventType.
+	 */
+	List<JobEvent> findByType(JobEventType type);
+	
+}
