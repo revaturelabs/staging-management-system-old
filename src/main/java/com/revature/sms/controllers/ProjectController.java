c58789bd37d955cc7d4bf9667c87274297644da4
Juan Carmona <Juan Carmona>
2017-02-07 16:11:24 -0500

    projects can now be deleted

--- src/main/java/com/revature/sms/controllers/ProjectController.java
+++ src/main/java/com/revature/sms/controllers/ProjectController.java
@@ -5,14 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.sms.domain.Project;
 import com.revature.sms.domain.ProjectUser;
 import com.revature.sms.domain.dao.ProjectRepo;
 import com.revature.sms.domain.dao.ProjectUserRepo;
+import com.revature.sms.domain.dto.bc;
 
 
 @RestController
@@ -29,6 +34,23 @@ public class ProjectController {
 	public Object getAll() {
 		List<Project> list = pr.findAll();
 		return new ResponseEntity<List<Project>>(list, HttpStatus.OK);
+	}
+	
+	//updates all inputed projects
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody Object update(@RequestHeader(value = "Authorization") String token, @RequestBody List<Project> projects) {
+		
+		pr.save(projects);
+		
+		return new ResponseEntity<bc>(new bc(true), HttpStatus.OK);
+	}
+	
+	//deletes all inputed projects
+	@RequestMapping(value="/remove",method = RequestMethod.POST)
+	public @ResponseBody Object remove(@RequestHeader(value = "Authorization") String token, @RequestBody List<Project> projects) {
+		pr.delete(projects);
+		
+		return new ResponseEntity<bc>(new bc(true), HttpStatus.OK);
 	}
 	
 	//returns all ProjectUsers
--- src/main/resources/js/controllers/manager/superadmin/updateProjects.js
+++ src/main/resources/js/controllers/manager/superadmin/updateProjects.js
@@ -5,22 +5,26 @@ function updateProjectsCtrl( $scope, $mdDialog, userService, projectService) {
 	var upc = this;
 	
 	//bindables
-	/**@prop {string} message Variable with message to be displayed*/
+	/**@prop {string} message Variable with message to be displayed.*/
 	upc.message = "";
+	/**@prop {object} selectedProject Variable with selected project.*/
 	upc.selectedProject = {};
+	upc.errors = [];
+	upc.toDelete = [];
 
 	//functions
+	/**@var {function} getProjects function reference variable. */
 	upc.getProjects = getProjects;
+	/**@var {function} newProject function reference variable. */
 	upc.newProject = newProject;
+	/**@var {function} deleteProject function reference variable. */
 	upc.deleteProject = deleteProject;
-	
 	/**@var {function} assignSubmit function reference variable. */
 	upc.assignSubmit = assignSubmit;
 	/**@var {function} assignCancel function reference variable. */
 	upc.assignCancel = assignCancel;
 	/**@var {function} close function reference variable. */
 	upc.close = close;
-	
 	
 	//initialization
 	upc.getProjects();
@@ -40,17 +44,16 @@ function updateProjectsCtrl( $scope, $mdDialog, userService, projectService) {
     			upc.allProjects[i].startDate = new Date(upc.allProjects[i].startDate);
     			upc.allProjects[i].endDate = new Date(upc.allProjects[i].endDate);
     			upc.allProjects[i].displayName = upc.allProjects[i].name;
-    			
     		}
-    		
     	}, function(error) {
     	})
 	}
-	
+	var count=0;
 	function newProject(){
-		upc.allProjects.push({"name":"New project","displayName":"New project"});
+		upc.allProjects.push({"name":"New project" + count,"displayName":"New project" + count});
 		upc.selectedProject = upc.allProjects[upc.allProjects.length-1];
 		upc.message = "Project added";
+		count++;
 	}
 	
 	function deleteProject(){
@@ -61,10 +64,8 @@ function updateProjectsCtrl( $scope, $mdDialog, userService, projectService) {
 		
 		for(var i = 0; i < upc.allProjects.length; i++){
 			if(upc.allProjects[i].name == upc.selectedProject.name){
-				upc.allProjects[i].name ="";
-				upc.allProjects[i].startDate = "";
-				upc.allProjects[i].endDate = "";
-				upc.allProjects[i].description = "";
+				upc.toDelete.push(upc.allProjects[i]);
+				
 				upc.selectedProject = {};
 				
 				upc.allProjects.splice(i,1);
@@ -74,7 +75,58 @@ function updateProjectsCtrl( $scope, $mdDialog, userService, projectService) {
 	}
 	
 	function assignSubmit(){
-		$mdDialog.cancel();
+		var pass = true;
+		upc.errors = [];
+		for(var i = 0; i < upc.allProjects.length; i++){
+			//validation
+			
+			//no start date
+			if(!upc.allProjects[i].startDate){
+				upc.errors.push({ 
+					"name" : upc.allProjects[i].name,
+					"msg": "Error in project: " + upc.allProjects[i].name +  ", no end date"
+					});
+				pass = false;
+			}
+			
+			//no end date
+			if(!upc.allProjects[i].endDate){
+				upc.errors.push({ 
+					"name" : upc.allProjects[i].name,
+					"msg": "Error in project: " + upc.allProjects[i].name +  ", no start date"
+					});
+				pass = false;
+					
+			}
+			
+			//end dates are after start dates
+			if(upc.allProjects[i].endDate.getTime() < upc.allProjects[i].startDate.getTime()){
+				var string = "Error in project: " + upc.allProjects[i].name + ", end date is before start date.";
+				
+				upc.errors.push({
+					"name" : upc.allProjects[i].name,
+					"msg": string
+					});
+				
+				pass = false;
+			}
+			
+		}
+		
+		if(pass){
+			
+			for(var s = 0; s<upc.allProjects;s++){
+				upc.allProject[s].name = upc.allProject[s].displayName;
+			}
+			
+			if(upc.toDelete.length > 0){
+				console.log(upc.toDelete);
+				projectService.del(upc.toDelete,function(){console.log("success");});
+			}
+			projectService.update(upc.allProjects,function(){console.log("success"); $mdDialog.cancel();});
+			
+		}
+		
 	}
 	
 	function assignCancel(){
