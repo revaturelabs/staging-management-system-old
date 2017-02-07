8cea7177fe9495b5e480cf042dca3d4abbe3abb5
Joseph Tamberino <jtamber2@gmail.com>
2017-02-07 16:48:22 -0500

    end of day commit

--- src/main/resources/js/factories/skillEditFactory.js
+++ src/main/resources/js/factories/skillEditFactory.js
@@ -9,7 +9,9 @@ function skillEditFac(){
     var skillsAddFail = [];
     var skillsRemoveSuccess = [];
     var skillsRemoveFail = [];
-
+    /**
+     * @prop {obj} service Service returned by the factory.
+     */
     var service = {
         addToAddSuccees: addToAddSuccees,
         addToAddFail: addToAddFail,
@@ -24,40 +26,59 @@ function skillEditFac(){
     return service;
 
     //functions
+    /**
+     * @description Function that adds the skill to the skillsAddSuccess array.
+     */
     function addToAddSuccees(skillName){
         skillsAddSuccess.push(skillName);
     }
-
+    /**
+     * @description Function that adds the skill to the skillsAddFail array.
+     */
     function addToAddFail(skillName, error){
         var obj = {skill: skillName, error: error};
         skillsAddFail.push(obj);
     }
-
+    /**
+     * @description Function that adds the skill to the skillsRemoveSuccess array.
+     */
     function addToRemoveSuccess(skillName){
         skillsRemoveSuccess.push(skillName);
     }
-
+    /**
+     * @description Function that adds the skill to the skillsRemoveFail array.
+     */
     function addToRemoveFail(skillName, error){
         var obj = {skill: skillName, error: error};
         skillsRemoveFail.push(obj);
     }
-
+    /**
+     * @description Function that returns the skillsAddSuccess array.
+     */
     function getAddSuccess(){
         return skillsAddSuccess;
     }
-
+    /**
+     * @description Function that returns the skillsAddFail array.
+     */
     function getAddFail(){
         return skillsAddFail;
     }
-
+    /**
+     * @description Function that returns the skillsRemoveSuccess array.
+     */
     function getRemoveSuccess(){
         return skillsRemoveSuccess;
     }
-
+    /**
+     * @description Function that returns the skillsRemoveFail array.
+     */
     function getRemoveFail(){
         return skillsRemoveFail;
     }
-
+    /**
+     * @description Function that clears all of the arrays.
+     */
     function clearAll(){
         skillsAddSuccess.length = 0;
         skillsAddFail.length = 0;
